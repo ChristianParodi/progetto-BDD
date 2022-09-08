659d136223f3721ff1dcf2e2f48938278c1addb5
Christian <christian.parodi19@gmail.com>
2022-09-08 17:41:30 +0200

    Implementati i trigger

--- parte_2/triggers.sql
+++ parte_2/triggers.sql
@@ -1,47 +1,66 @@
--- Aggiornamento inventario
+-- Trigger che mantiene aggiornata la quantita' dei prodotti in inventario
+-- aggiungendone uno ogni volta che viene inserita una tupla in prodotti
+CREATE OR REPLACE FUNCTION addQta() RETURNS TRIGGER AS
+$add_qta$
+    BEGIN
+       UPDATE scorte SET qta = qta + 1 WHERE codice_prodotto = NEW.codice_prodotto;
+       RETURN NULL;
+    END;
+$add_qta$ LANGUAGE plpgsql;
 
--- Aggiungi la quantita' quando viene inserita una tupla in prodotti
--- Sottrai la quantita' quando viene inserita una tupla in appuntamenti_prodotti (il prodotto viene comprato)
+CREATE TRIGGER add_qta
+AFTER INSERT ON prodotti
+FOR EACH ROW EXECUTE FUNCTION addQta();
 
--- Vincoli d'integrita'
+-- ad ogni inserimento in appuntamenti_prodotti, togli la quantita' in scorte
+CREATE OR REPLACE FUNCTION subQta() RETURNS TRIGGER AS
+$sub_qta$
+    DECLARE
+            id_scorta INT;
+    BEGIN
+        SELECT codice_prodotto FROM prodotti WHERE ID = NEW.prodotto INTO id_scorta;
+        UPDATE scorte SET qta = qta - 1 WHERE codice_prodotto = id_scorta;
+        RETURN NULL;
+    END;
+$sub_qta$ LANGUAGE plpgsql;
 
--- prima di assegnare un volontario ad un turno trasporti, controllare se il volontario e' gia' assegnato ad un altro turno in quella
--- fascia oraria oppure se e' disponibile per il servizio. 
--- diminuire la quantita' dei prodotti quando questi vengono scaricari
--- per le fasce d'eta' dei familiari inserire automaticamente la fascia d'eta' corrispondente all'eta'
--- per i clienti, settare automaticamente la scadenza dell'autorizzazione a 6 mesi dalla data di autorizzazione
+CREATE TRIGGER sub_qta
+AFTER INSERT ON appuntamenti_prodotti
+FOR EACH ROW EXECUTE FUNCTION subQta();
 
--- Scarico dei prodotti
+-- Verifica del vincolo che ogni volontario non si assegnato a piu' attivita'
+-- contemporaneamente
 
--- La nostra idea era di eseguire una data funzione ogni giorno ad un certo orario, che controllasse quali prodotti 
--- sono da scaricare nella data odierna e quindi rimuoverli dal DB (mantenendo consistente l'inventario). Siccome pero'
--- non e' possibile effettuare questa operazione in postgre, l'ideale sarebbe creare una normale funzione e poi farla eseguire ad
--- un job scheduler (che non faremo). Ci limiteremo quindi a creare una funzione che effettua lo scarico dei prodotti dal db 
--- (quindi che cancella materialmente le tuple), spettera' al volontario eseguire poi questa funzione.
-
-CREATE OR REPLACE FUNCTION scarico(data1 DATE DEFAULT CURRENT_DATE, data2 DATE DEFAULT CURRENT_DATE + '1 month'::interval) 
-RETURNS void AS
-$scarico$
-    -- In questo caso e' necessario che data1 sia prima di data2
-    IF data1 > data2 THEN
-        RAISE EXCEPTION "la prima data deve essere precedente alla seconda";
-    END IF;
-    -- E' necessario salvare solamente l'ID del prodotto per poterlo eliminare
-    DECLARE 
-        id_prodotto INT;
+CREATE OR REPLACE FUNCTION checkTurniVol() RETURNS TRIGGER AS
+$check_turni_volontari$
+    DECLARE
+        volTurni turni;
         cur CURSOR FOR
-            SELECT id
-            FROM prodotti
-            WHERE data_scarico BETWEEN data1 AND data2;
+            SELECT *
+            FROM turni
+            WHERE id = 1 and data = ANY (
+                SELECT data
+                FROM turni
+                WHERE id = 1
+            );
     BEGIN
         OPEN cur;
-        FETCH cur INTO id_prodotto;
+        FETCH cur INTO volTurni;
+
         WHILE FOUND LOOP
             BEGIN
-                DELETE FROM prodotti WHERE id = id_prodotto;
-                FETCH cur INTO id_prodotto;
+                IF (NEW.ora_inizio, NEW.ora_fine) OVERLAPS (volTurni.ora_inizio, volTurni.ora_fine)
+                THEN
+                    RAISE NOTICE 'Errore: il turno da inserire si sovrappone al turno (%, %, %)', volTurni.data, volTurni.ora_inizio, volTurni.ora_fine;
+                    RETURN NULL;
+                END IF;
+               FETCH cur INTO volTurni;
             END;
         END LOOP;
         CLOSE cur;
     END;
-$scarico$ LANGUAGE plpgsql;
+$check_turni_volontari$ LANGUAGE plpgsql;
+
+CREATE TRIGGER checkTurniVolontari
+BEFORE INSERT ON volontari_turni
+FOR EACH ROW EXECUTE FUNCTION checkTurniVol();
