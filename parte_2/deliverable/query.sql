6e9032029cc3382e9b6f734ee69cfb14eb97e64b
Christian <christian.parodi19@gmail.com>
2022-09-12 15:52:19 +0200

    Aggiustata la query 2

--- parte_2/deliverable/query.sql
+++ parte_2/deliverable/query.sql
@@ -13,8 +13,18 @@ WHERE id NOT IN (
 -- determinare le tipologie di prodotti acquistate nell’ultimo anno da tutte le famiglie (cioè ogni fa-
 -- miglia ha acquistato almeno un prodotto di tale tipologia nell’ultimo anno);
 
+-- In pratica, per implementare la divisione ci siamo ricavati
+-- una tabella intermedia (R) che e' la tabella di ogni acquisto
+-- effettuato durante l'anno con cliente e tipologia dell'acquisto,
+-- da questa contiamo quante volte compare una data tipologia per ogni cliente (distinto)
+-- e se questo numero e' uguale al numero di clienti (distinti) che hanno acquistato qualcosa
+-- nell'anno precedente allora posso affermare che quella tipologia e' stata acquistata da tutti.
+
+SELECT tipologia
+FROM (
 SELECT R.tipologia, COUNT(R.tipologia)
-FROM (SELECT c.id as id_famiglia, s.tipologia
+FROM (
+    SELECT c.id as id_famiglia, s.tipologia
     FROM scorte s
     NATURAL JOIN prodotti p
     JOIN appuntamenti_prodotti ap on p.id = ap.prodotto
@@ -22,7 +32,10 @@ FROM (SELECT c.id as id_famiglia, s.tipologia
     JOIN clienti c on a.cliente = c.id
     WHERE a.data >= CURRENT_DATE - '1 year'::interval) AS R
 GROUP BY R.tipologia
-HAVING COUNT(R.id_famiglia) = (SELECT COUNT(*) FROM clienti)
+HAVING COUNT(DISTINCT R.id_famiglia) = (SELECT COUNT(DISTINCT cliente)
+                               FROM clienti
+                               JOIN appuntamenti a2 on clienti.id = a2.cliente
+                               WHERE a2.data >= CURRENT_DATE - '1 year'::interval)) AS res;
 
 -- determinare i prodotti che vengono scaricati (cioè non riescono ad essere distribuiti alle famiglie)
 -- in quantitativo maggiore rispetto al quantitativo medio scaricato per prodotti della loro tipologia
